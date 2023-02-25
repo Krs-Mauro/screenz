3fb3ea380ee689d9a36820b8aad2dba07017704e
Mauro Kraus <krs.mauro@gmail.com>
2023-02-25 16:59:16 -0500

    Adds reducer, components scafold and firebase

---
+++ src/components/body/RowElement.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RowElement = () => {};
+
+export default RowElement;
---
+++ src/redux/mediaSlice.ts
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface MediaState {
+  mediaType: "show" | "movie";
+  movies: any[];
+  shows: any[];
+  isBodyLoading: boolean;
+  isModalLoading: boolean;
+  isModalOpen: boolean;
+  selectedItem: any;
+}
+
+const initialState: MediaState = {
+  mediaType: "show",
+  movies: [],
+  shows: [],
+  isBodyLoading: false,
+  isModalLoading: false,
+  isModalOpen: false,
+  selectedItem: {},
+};
+
+export const mediaSlice = createSlice({
+  name: "media",
+  initialState,
+  reducers: {
+    setMediaType: (state, action) => {
+      state.mediaType = action.payload;
+    },
+    setMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+    setShows: (state, action) => {
+      state.shows = action.payload;
+    },
+    setIsBodyLoading: (state, action) => {
+      state.isBodyLoading = action.payload;
+    },
+    setIsModalLoading: (state, action) => {
+      state.isModalLoading = action.payload;
+    },
+    setIsModalOpen: (state, action) => {
+      state.isModalOpen = action.payload;
+    },
+    setSelectedItem: (state, action) => {
+      state.selectedItem = action.payload;
+    },
+  },
+});
+
+export const {
+  setMediaType,
+  setMovies,
+  setShows,
+  setIsBodyLoading,
+  setIsModalLoading,
+  setIsModalOpen,
+  setSelectedItem,
+} = mediaSlice.actions;
+
+export default mediaSlice.reducer;
