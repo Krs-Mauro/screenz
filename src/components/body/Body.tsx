14a75c56287b9c5dae153c4020176bbe0ace8b40
Mauro Kraus <krs.mauro@gmail.com>
2023-02-26 15:20:22 -0500

    Adds logic and styles to the modal content

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -2,21 +2,27 @@ import React, { useRef } from "react";
 
 import { Stack } from "@mui/system";
 import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { RootState } from "../../redux/store";
+import { setIsModalOpen } from "../../redux/mediaSlice";
 
 import MovieRow from "./MovieRow";
 import ShowRow from "./ShowRow";
 import { useFetchMovies, useFetchShows } from "./bodyHelpers";
 import TableHeaders from "./TableHeaders";
+import DetailModal from "./DetailModal";
 
 const Body = () => {
   const mediaType = useSelector((state: RootState) => state.media.mediaType);
   const movies = useSelector((state: RootState) => state.media.movies);
   const shows = useSelector((state: RootState) => state.media.shows);
+  const open = useSelector((state: RootState) => state.media.isModalOpen);
   const calledMovies = useRef<boolean>(false);
   const calledShows = useRef<boolean>(false);
+  const dispatch = useDispatch();
+
+  const onClose = () => dispatch(setIsModalOpen(false));
 
   useFetchMovies(calledMovies);
   useFetchShows(calledShows);
@@ -36,6 +42,8 @@ const Body = () => {
 
       {mediaType === "show" &&
         shows.map((show) => <ShowRow key={show.id} element={show} />)}
+
+      <DetailModal open={open} onClose={onClose} />
     </Stack>
   );
 };
--- src/components/body/DetailModal.tsx
+++ src/components/body/DetailModal.tsx
@@ -1,5 +1,76 @@
 import React from "react";
 
-const DetailModal = () => {};
+import { Stack } from "@mui/system";
+import { Modal, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../../redux/store";
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+
+const stackStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  padding: "40px",
+  borderRadius: "8px",
+  backgroundColor: "#F3EFE6",
+  border: "none",
+  outline: "none",
+  width: "80%",
+};
+
+const DetailModal = ({ open, onClose }: Props) => {
+  const selectedItem = useSelector(
+    (state: RootState) => state.media.selectedItem
+  );
+  const mediaType = useSelector((state: RootState) => state.media.mediaType);
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Stack sx={stackStyle}>
+        {mediaType === "movie" ? (
+          <Typography variant="h2" marginBottom="32px">
+            {selectedItem.title}
+          </Typography>
+        ) : (
+          <Typography variant="h2" marginBottom="32px">
+            {selectedItem.name}
+          </Typography>
+        )}
+        <Stack direction="row" marginBottom="32px">
+          <img
+            src={selectedItem.big_backdrop_path}
+            alt={`Poster for the ${selectedItem.title} show`}
+            style={{
+              maxWidth: "60%",
+              objectFit: "contain",
+              borderRadius: "8px",
+              marginTop: "3px",
+            }}
+          />
+          <Stack margin="0 0 0 40px" justifyContent="center">
+            <Typography variant="h3">
+              {selectedItem.vote_average} / 10
+            </Typography>
+            <Typography variant="h5">
+              First aired on:{" "}
+              {mediaType === "movie"
+                ? selectedItem.release_date
+                : selectedItem.first_air_date}
+            </Typography>
+            <Typography variant="h6" marginTop="32px">
+              {selectedItem.overview}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Modal>
+  );
+};
 
 export default DetailModal;
